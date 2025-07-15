3f16912f224638498b5f75d11b0cde562dd5751f
dnor-dev <anorue4rss@hotmail.com>
2025-07-15 02:22:16 +0100

    feat: dashboard layout

---
+++ app/gateways/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Gateways = () => {
+  return (
+    <div className="flex flex-col space-y-5">
+      <div className="flex items-center space-x-4">
+        <img src="/icons/gateway-header.svg" alt="Gateways" />
+        <h1 className="text-[32px] font-semibold text-white">Gateways</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Gateways;
