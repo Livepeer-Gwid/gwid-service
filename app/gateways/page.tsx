27367241b287da63465ae4627ef052085c98f65b
dnor-dev <anorue4rss@hotmail.com>
2025-07-16 11:11:38 +0100

    chore: gateway cards

--- app/gateways/page.tsx
+++ app/gateways/page.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { List, LayoutGrid, Plus } from "lucide-react";
+import SearchGateway from "@/components/gateway/search-gateway";
+import GatewayCard from "@/components/gateway/gateway-card";
 
 const Gateways = () => {
   return (
-    <div className="flex flex-col space-y-5">
+    <div className="w-full flex flex-col space-y-9">
       <div className="flex items-center space-x-4">
         <img src="/icons/gateway-header.svg" alt="Gateways" />
         <h1 className="text-[32px] font-semibold text-white">Gateways</h1>
+      </div>
+
+      <div className="flex flex-col space-y-7">
+        <div className="flex items-center space-x-7 h-11">
+          <SearchGateway />
+
+          <div className="flex items-center bg-[#171B2080] border border-[#383A3F] rounded-[10px] h-11">
+            <Button
+              type="button"
+              variant="ghost"
+              className="px-3 h-full border-r border-[#2C2C2E] text-[#FFFFFFB2] hover:bg-[#6C5CE71A]"
+            >
+              <LayoutGrid size={18} />
+            </Button>
+            <Button
+              type="button"
+              variant="ghost"
+              className="px-3 h-full text-[#FFFFFFB2] hover:bg-[#6C5CE71A]"
+            >
+              <List size={18} />
+            </Button>
+          </div>
+
+          <Button className="h-full">
+            <Plus className="mr-2" /> Launch New Gateway
+          </Button>
+        </div>
+
+        <div className="w-full grid grid-cols-3 gap-6 pb-7">
+          {[...Array(10)].map((_, i) => (
+            <GatewayCard key={i} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ components/gateway/no-gateway-launched.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
+
+const NoGatewayLaunched = () => {
+  return (
+    <div className="relative w-full h-full pt-3">
+      <img
+        src="/images/gateway-empty-bg.jpg"
+        className="w-full h-full object-cover rotate-180 absolute rounded-[20px]"
+        alt="Gateways"
+      />
+
+      <div className="w-full p-10 relative flex flex-col space-y-9">
+        <div className="flex flex-col space-y-3.5">
+          <h4 className="text-lg font-semibold text-white">
+            No gateways have been launched yet
+          </h4>
+          <p className="text-[#FFFFFF80] font-medium">
+            Get started by launching a gateway.
+          </p>
+        </div>
+
+        <Button
+          variant="outline"
+          className="bg-inherit text-white font-semibold rounded-[30px] border-[#3A3C41] w-fit h-10 px-5"
+        >
+          Launch a new gateway <ArrowRight size={18} className="ml-2" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NoGatewayLaunched;
