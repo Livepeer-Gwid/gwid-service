2f8cd2daecf0fdaa39e925295219cb16bd572d3e
dnor-dev <anorue4rss@hotmail.com>
2025-07-24 00:55:34 +0100

    fix: fix layout scroll issue

--- components/layouts/dashboard-layout.tsx
+++ components/layouts/dashboard-layout.tsx
@@ -10,22 +10,22 @@ const nunito = Nunito({
 
 const DashboardLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-app-surface h-screen overflow-y-auto no-scrollbar">
-      <div className="flex w-full h-full">
-        <Sidebar />
-        <div className="w-full h-full flex flex-col overflow-auto no-scrollbar py-5">
-          <div className="w-full flex justify-end items-center space-x-7 px-7">
-            <button className="flex items-center gap-2 text-[#FFFFFFB2] transition-colors duration-200">
-              <HelpCircle className="w-5 h-5" strokeWidth={1.5} />
-              <span className={`font-medium ${nunito.className}`}>Help</span>
-            </button>
-            <button className="flex items-center gap-2 text-[#FFFFFFB2] transition-colors duration-200">
-              <UserCircle className="w-5 h-5" strokeWidth={1.5} />
-              <span className={`font-medium ${nunito.className}`}>Account</span>
-            </button>
-          </div>
-          <main className={`px-10 ${nunito.className} h-full`}>{children}</main>
+    <div className="bg-app-surface h-full flex">
+      <Sidebar />
+      <div className="w-full h-full flex flex-col py-5">
+        <div className="w-full flex justify-end items-center space-x-7 px-7">
+          <button className="flex items-center gap-2 text-[#FFFFFFB2] transition-colors duration-200">
+            <HelpCircle className="w-5 h-5" strokeWidth={1.5} />
+            <span className={`font-medium ${nunito.className}`}>Help</span>
+          </button>
+          <button className="flex items-center gap-2 text-[#FFFFFFB2] transition-colors duration-200">
+            <UserCircle className="w-5 h-5" strokeWidth={1.5} />
+            <span className={`font-medium ${nunito.className}`}>Account</span>
+          </button>
         </div>
+        <main className={`px-10 ${nunito.className} h-full ml-[20%]`}>
+          {children}
+        </main>
       </div>
     </div>
   );
